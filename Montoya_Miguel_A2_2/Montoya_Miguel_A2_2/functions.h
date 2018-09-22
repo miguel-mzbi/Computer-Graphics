0dcb49efa65a539e1cdd3cf0cf204945d2e8a060
Miguel Angel Montoya <miguel.mzbi@gmail.com>
2018-09-22 13:14:03 -0400

    Missing translation after rotation

---
+++ Montoya_Miguel_A2_2/Montoya_Miguel_A2_2/functions.h
@@ -0,0 +1,8 @@
+#pragma once
+
+static void draw();
+static void clearBothBuffers();
+static void refresh();
+static void displayTea();
+static void screenToPlaneCoords(int, int, std::vector<float> &);
+static float computeAngleLocal();
